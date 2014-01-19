9c4daa1bba32b330b2253439d3bb5487fe58e316
Dave Moten <davidmoten@gmail.com>
2014-01-19 22:57:50 +1100

    ws dev

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,6 +9,7 @@ import play.api.libs.iteratee.Iteratee
 import play.api.libs.iteratee.Enumerator
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.iteratee.Concurrent
+import scala.sys.process._
 
 object Application extends Controller {
 
@@ -21,7 +22,9 @@ object Application extends Controller {
     val (out, channel) = Concurrent.broadcast[String]
     val in = Iteratee.foreach[String] {
       msg =>
-        channel.push(msg)
+        Process(msg)
+          .lines
+          .foreach(channel.push(_))
     }
     (in, out)
   }
