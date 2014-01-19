d70405a806415b001dfa5902f6a73895c929bbc4
Dave Moten <davidmoten@gmail.com>
2014-01-19 22:18:15 +1100

    add websockets echo example at /

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,17 +2,29 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-
 import models.Task
-
 import play.api.data._
 import play.api.data.Forms._
+import play.api.libs.iteratee.Iteratee
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.iteratee.Concurrent
 
 object Application extends Controller {
 
-  def index = Action {
-    Redirect(routes.Application.tasks)
+//  def index = Action {
+//    Redirect(routes.Application.tasks)
+//  }
+  
+  def index = WebSocket.using[String] { request => 
+  
+  val (out,channel) = Concurrent.broadcast[String]
+  val in = Iteratee.foreach[String] {
+    msg =>
+      channel.push(msg)
   }
+  (in, out)
+}
 
   def tasks = Action {
     Ok(views.html.index(Task.all(), taskForm))
